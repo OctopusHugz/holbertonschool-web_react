e99e2bc2cd26050a73ecd47d1981316f5c1aaebb
OctopusHugz <colsonscott53@gmail.com>
2021-06-21 22:40:40 +0000

    Added js/main.ts

---
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -0,0 +1,17 @@
+interface MajorCredits {
+	brand: 'major';
+	credits: number;
+}
+
+interface MinorCredits {
+	brand: 'minor';
+	credits: number;
+}
+
+function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
+	return subject1.credits + subject2.credits;
+}
+
+function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
+	return subject1.credits + subject2.credits;
+}
