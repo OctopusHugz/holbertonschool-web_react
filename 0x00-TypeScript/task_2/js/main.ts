0fd8c25ff0b51a863318251edde2020165410704
OctopusHugz <colsonscott53@gmail.com>
2021-06-21 19:30:01 +0000

    Added isDirector and executeWork functions

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -32,3 +32,12 @@ createEmployee = function (salary: number | string): Director | Teacher {
 	if (typeof salary === 'number' && salary < 500) return new Teacher();
 	return new Director();
 }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+	return (employee as Director).workDirectorTasks !== undefined;
+}
+
+function executeWork(employee: Director | Teacher): void {
+	if (isDirector(employee)) console.log(employee.workDirectorTasks());
+	else console.log(employee.workTeacherTasks());
+}
