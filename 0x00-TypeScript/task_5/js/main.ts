e7655c2cc2e40f295be6b7ac99d007d3a971b5f4
OctopusHugz <colsonscott53@gmail.com>
2021-06-22 07:39:12 +0000

    Changed credits to credit for test file

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,17 +1,17 @@
 export interface MajorCredits {
 	brand: 'major';
-	credits: number;
+	credit: number;
 }
 
 export interface MinorCredits {
 	brand: 'minor';
-	credits: number;
+	credit: number;
 }
 
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
-	return subject1.credits + subject2.credits;
+	return subject1.credit + subject2.credit;
 }
 
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
-	return subject1.credits + subject2.credits;
+	return subject1.credit + subject2.credit;
 }
