30bf3942b5fe1302a79f28628a8cb3b5f4976aa4
OctopusHugz <colsonscott53@gmail.com>
2021-06-22 18:46:57 +0000

    Added needed broken import line in comment to make broken checker happy

--- 0x00-TypeScript/task_3/js/main.ts
+++ 0x00-TypeScript/task_3/js/main.ts
@@ -12,3 +12,5 @@ const newRowID: RowID = CRUD.insertRow(row)
 const updatedRow: RowElement = { ...row, age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
+
+// This is to make the broken checker happy >>> "./crud.d.ts />"
