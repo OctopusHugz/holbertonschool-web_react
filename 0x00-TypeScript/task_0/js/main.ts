f26f05f6383c9ef461adc16b967b5f9bb301a955
OctopusHugz <colsonscott53@gmail.com>
2021-06-22 06:39:32 +0000

    Try adding void return type to forEach

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -23,7 +23,7 @@ const studentsList: Student[] = [student1, student2];
 const table: HTMLTableElement = document.createElement('table');
 document.body.appendChild(table);
 
-studentsList.forEach((student: Student) => {
+studentsList.forEach((student: Student): void => {
 	const newRow: HTMLTableRowElement = table.insertRow();
 	const newRowFirstName: HTMLTableCellElement = newRow.insertCell();
 	const newRowLocation: HTMLTableCellElement = newRow.insertCell();
