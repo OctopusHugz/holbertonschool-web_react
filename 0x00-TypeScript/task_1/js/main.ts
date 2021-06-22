8b97521566c66b760a1e2ff78a3911846599df5f
OctopusHugz <colsonscott53@gmail.com>
2021-06-22 07:11:16 +0000

    Added period back to return value for printTeacher

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -16,7 +16,7 @@ export interface printTeacherFunction {
 }
 
 export const printTeacher: printTeacherFunction = function (firstName: string, lastName: string): string {
-	return `${firstName[0]} ${lastName}`;
+	return `${firstName[0]}. ${lastName}`;
 }
 
 export class StudentClass implements StudentClassInterface {
