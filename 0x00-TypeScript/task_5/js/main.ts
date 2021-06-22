8f9fba9be745cf356fe1be29f4a72d5bb3f6abb0
OctopusHugz <colsonscott53@gmail.com>
2021-06-22 21:03:27 +0000

    Changed __brand to _brand and changed return

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,19 +1,19 @@
 export interface MajorCredits {
-	__brand: 'major';
+	_brand: 'major';
 	credits: number;
 }
 
 export interface MinorCredits {
-	__brand: 'minor';
+	_brand: 'minor';
 	credits: number;
 }
 
 export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits) {
-	return subject1.credits + subject2.credits;
+	return { credits: subject1.credits + subject2.credits } as MajorCredits;
 }
 
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits) {
-	return subject1.credits + subject2.credits;
+	return { credits: subject1.credits + subject2.credits } as MinorCredits;
 }
 
 // credit: number;
