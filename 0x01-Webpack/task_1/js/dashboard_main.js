216c929311e5c4d7d9bbbc9a4625da1a739d6fd0
OctopusHugz <colsonscott53@gmail.com>
2021-06-23 16:44:18 +0000

    Added js/dashboard_main.js

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$('<p>Holberton Dashboard</p>').appendTo('body');
+$('<p>Dashboard data for the students</p>').appendTo('body');
+$('<button>Click here to get started</button>').appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$('<p>Copyright - Holberton School</p>').appendTo('body');
