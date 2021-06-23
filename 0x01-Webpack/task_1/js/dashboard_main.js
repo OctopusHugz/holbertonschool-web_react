99388df4664b79c8726f553fe370c974eaa7cb7e
OctopusHugz <colsonscott53@gmail.com>
2021-06-23 17:03:15 +0000

    Added updates for button and updateCounter func

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -5,3 +5,12 @@ $('<p>Dashboard data for the students</p>').appendTo('body');
 $('<button>Click here to get started</button>').appendTo('body');
 $("<p id='count'></p>").appendTo('body');
 $('<p>Copyright - Holberton School</p>').appendTo('body');
+
+$('button').on('click', _.debounce(updateCounter));
+
+let count = 0;
+
+function updateCounter() {
+	count += 1;
+	$('#count').html(`${count} clicks on the button`);
+}
