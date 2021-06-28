e685c58ca444e5d74960c9a8062634066bd1ba2d
OctopusHugz <colsonscott53@gmail.com>
2021-06-28 22:53:12 +0000

    Changed header body and footer to divs

--- 0x02-react_intro/task_0/dashboard/src/App.js
+++ 0x02-react_intro/task_0/dashboard/src/App.js
@@ -4,18 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
-      </header>
+      </div>
       <hr />
-      <body className="App-body">
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
-      </body>
+      </div>
       <hr />
-      <footer className="App-footer">
+      <div className="App-footer">
         <p>Copyright 2020 - holberton School</p>
-      </footer>
+      </div>
     </div>
   );
 }
