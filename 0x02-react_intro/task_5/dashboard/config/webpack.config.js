fc79f4640870b614fd200b8e98ea19d07b0c4d35
OctopusHugz <colsonscott53@gmail.com>
2021-06-29 19:46:23 +0000

    Added babel-loader to webpack.config.js

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -44,6 +44,18 @@ module.exports = {
 						},
 					},
 				],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: [
+							['@babel/preset-env', { targets: "defaults" }]
+						]
+					}
+				}
 			}
     ],
 	},
