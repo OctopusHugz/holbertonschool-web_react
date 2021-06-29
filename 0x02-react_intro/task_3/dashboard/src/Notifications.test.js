547003997437412931809dfff79749f2792000da
OctopusHugz <colsonscott53@gmail.com>
2021-06-29 18:25:42 +0000

    Added test files for App and Notifications

---
+++ 0x02-react_intro/task_3/dashboard/src/Notifications.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Notifications from './Notifications'
+
+// verify that Notifications renders three list items
+// verify that Notifications renders the text Here is the list of notifications
+
+// You must use shallow rendering to write the React tests
+
+describe('<Notifications />', () => {
+  it('renders an <Notifications /> component', () => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper).toHaveLength(1);
+	});
+
+  it('renders an <Notifications /> component and checks for 3 list items', () => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.find('.Notifications li')).toHaveLength(3);
+	});
+
+  it('renders an <Notifications /> component and verifies text', () => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.find('.Notifications p')).toHaveLength(1);
+    expect(wrapper.find('.Notifications p').text()).toEqual('Here is the list of notifications')
+	});
+});
