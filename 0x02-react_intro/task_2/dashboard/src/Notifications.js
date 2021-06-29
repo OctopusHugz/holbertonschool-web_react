df7189132b9c42bb10367a6cb6bb033086622378
OctopusHugz <colsonscott53@gmail.com>
2021-06-29 17:48:13 +0000

    Added updates to Notifications component

--- 0x02-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Notifications.css'
+import icon from './close-icon.png'
+import { getLatestNotification } from './utils'
 
 const Notifications = () => {
+	function clickClose() {
+		console.log('Close button has been clicked')
+	}
+	const htmlObj = {
+		__html: getLatestNotification()
+	}
 	return (
 		<div className='Notifications'>
-			<p>Here is the list of notifications</p>
+			<p style={{ display: 'inline' }}>Here is the list of notifications</p>
+			<button style={{ float: 'right' }} aria-label="Close" onClick={clickClose}>
+				<img src={icon} alt="" style={{ height: '3vh'}} />
+			</button>
+			<ul>
+				<li data-priority="default">New course available</li>
+				<li data-priority="urgent">New resume available</li>
+				<li dangerouslySetInnerHTML={htmlObj} data-priority="urgent"></li>
+			</ul>
 		</div>
 	)
 }
