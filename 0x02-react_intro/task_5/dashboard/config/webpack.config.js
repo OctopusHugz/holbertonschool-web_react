93fe6447ff7e7831e0fc52fc4161436cb99c7f6b
OctopusHugz <colsonscott53@gmail.com>
2021-07-01 03:49:48 +0000

    Added updated webpack.config.js after setting context and fixing entry point

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-	entry: './src/index.js',
+	context: path.resolve(__dirname, '../src'),
+	entry: './index.js',
 	output: {
 		path: path.resolve('./public'),
 		filename: 'bundle.js',
