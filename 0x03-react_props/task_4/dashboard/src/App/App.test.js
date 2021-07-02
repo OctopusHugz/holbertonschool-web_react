f390d1ba4dcd44a3e8727a861a96a67152734757
OctopusHugz <colsonscott53@gmail.com>
2021-07-02 00:58:48 +0000

    Added tests for isLoggedIn and components

--- 0x03-react_props/task_4/dashboard/src/App/App.test.js
+++ 0x03-react_props/task_4/dashboard/src/App/App.test.js
@@ -5,6 +5,7 @@ import Notification from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
 
 describe('<App />', () => {
   it('renders an <App /> component', () => {
@@ -30,5 +31,16 @@ describe('<App />', () => {
   it('checks for a <Footer /> component', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find(Footer)).toHaveLength(1);
+	});
+
+  it('checks that <CourseList /> component is not displayed', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(CourseList)).toHaveLength(0);
+	});
+
+  it('checks component behavior when isLoggedIn === true', () => {
+    const wrapper = shallow(<App isLoggedIn={true}/>);
+    expect(wrapper.find(Login)).toHaveLength(0);
+    expect(wrapper.find(CourseList)).toHaveLength(1);
 	});
 });
