e1d838423d057502bca7116cc0594ff34ee020e9
OctopusHugz <colsonscott53@gmail.com>
2021-07-02 00:23:29 +0000

    Added prop types and default props

--- 0x03-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
+++ 0x03-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,12 +1,13 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
   return (
     <tr>
-      {isHeader && textSecondCell === undefined && (
+      {isHeader && textSecondCell === null && (
         <th colSpan={2}>{textFirstCell}</th>
       )}
-      {isHeader && textSecondCell !== undefined && (
+      {isHeader && textSecondCell !== null && (
         <>
           <th>{textFirstCell}</th>
           <th>{textSecondCell}</th>
@@ -20,6 +21,17 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
       )}
     </tr>
   );
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.string
+};
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null
 };
 
 export default CourseListRow;
