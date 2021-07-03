6ef1bbc957865f96286857462048a0d61cd19b44
OctopusHugz <colsonscott53@gmail.com>
2021-07-03 21:27:24 +0000

    Added listNotifications array and prop

--- 0x03-react_props/task_5/dashboard/src/App/App.js
+++ 0x03-react_props/task_5/dashboard/src/App/App.js
@@ -6,6 +6,7 @@ import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import CourseList from "../CourseList/CourseList";
 import PropTypes from 'prop-types';
+import { getLatestNotification } from "../utils/utils";
 
 const App = ({ isLoggedIn }) => {
   const listCourses = [
@@ -13,9 +14,17 @@ const App = ({ isLoggedIn }) => {
     {id: 2, name: 'Webpack', credit: 20},
     {id: 3, name: 'React', credit: 40},
   ];
+  const htmlObj = {
+    __html: getLatestNotification(),
+  };
+  const listNotifications = [
+    {id: 1, type: 'default', value: 'New course available'},
+    {id: 2, type: 'urgent', value: 'New resume available'},
+    {id: 3, type: 'urgent', html: htmlObj},
+  ];
   return (
     <>
-      <Notifications />
+      <Notifications listNotifications={listNotifications} />
       <div className="App">
         <Header />
         <hr />
