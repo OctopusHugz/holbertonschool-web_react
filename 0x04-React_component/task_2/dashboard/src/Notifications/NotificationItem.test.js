ebb13261b17835bcd08e47e6b340fd66ccfcc909
OctopusHugz <colsonscott53@gmail.com>
2021-07-13 16:46:03 +0000

    Added new test to verify markAsRead is called with correct ID when clicked

--- 0x04-React_component/task_2/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x04-React_component/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,5 +1,10 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import Notifications from './Notifications';
 import NotificationItem from './NotificationItem';
 
 describe('<NotificationItem />', () => {
@@ -20,4 +25,21 @@ describe('<NotificationItem />', () => {
 		const wrapper = shallow(<NotificationItem  html={{ __html: '<u>test</u>' }}/>);
 		expect(wrapper.html()).toContain('<u>test</u>');
 	});
+
+	it('verifies markAsRead is called with the right ID argument when clicked', () => {
+		const listNotifications = [
+      {id: 13, type: 'default', value: 'New course available'},
+      {id: 22, type: 'urgent', value: 'New resume available'},
+      {id: 53, type: 'urgent', value: 'Handle this now!!!'},
+    ];
+		const wrapper = mount(<Notifications displayDrawer={true} listNotifications={listNotifications} />);
+		const instance = wrapper.instance();
+		const spy = jest.spyOn(instance, 'markAsRead');
+		wrapper.find('li').first().simulate('click');
+		expect(spy).toBeCalledWith(13);
+		wrapper.find('li').at(1).simulate('click');
+		expect(spy).toBeCalledWith(22);
+		wrapper.find('li').last().simulate('click');
+		expect(spy).toBeCalledWith(53);
+	})
 });
