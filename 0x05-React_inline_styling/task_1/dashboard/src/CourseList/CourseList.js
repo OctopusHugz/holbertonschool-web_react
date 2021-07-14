e5212aa8a286582b8b46ec0fa40537e29d91a702
OctopusHugz <colsonscott53@gmail.com>
2021-07-14 17:10:18 +0000

    Added inline styles for CourseList and removed styling for it from CourseList.css

--- 0x05-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -3,10 +3,11 @@ import "./CourseList.css";
 import CourseListRow from "./CourseListRow";
 import CourseShape from "./CourseShape";
 import PropTypes from "prop-types";
+import { StyleSheet, css } from 'aphrodite';
 
 const CourseList = ({ listCourses }) => {
   return (
-    <table id="CourseList">
+    <table className={css(styles.courseListStyle)}>
       <thead>
         <CourseListRow textFirstCell="Available courses" isHeader={true} />
         <CourseListRow
@@ -39,5 +40,14 @@ CourseList.propTypes = {
 CourseList.defaultProps = {
   listCourses: [],
 };
+
+const styles = StyleSheet.create({
+  courseListStyle: {
+    width: '90%',
+    margin: '40px auto 150px auto',
+    minHeight: 150,
+    border: '1px solid grey',
+  }
+})
 
 export default CourseList;
