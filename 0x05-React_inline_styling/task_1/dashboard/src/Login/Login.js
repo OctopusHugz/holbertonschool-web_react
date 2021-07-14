2de325a44c6b92dbe291b3135d1972e5d9cef5e9
OctopusHugz <colsonscott53@gmail.com>
2021-07-14 17:19:53 +0000

    Changed to inline styles for Login component. Deleted Login.css

--- 0x05-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./Login.css";
+import { StyleSheet, css } from 'aphrodite';
 
 const Login = () => {
   return (
     <>
-      <div className="App-login">
-        <p>Login to access the full dashboard</p>
-        <form>
+      <div className={css(styles.loginStyle)}>
+        <p className={css(styles.loginPStyle)}>Login to access the full dashboard</p>
+        <form className={css(styles.loginFormStyle)}>
           <label>
             Email:
             <input type="text" name="email" />
@@ -21,5 +21,20 @@ const Login = () => {
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  loginStyle: {
+    minHeight: 150,
+    margin: '40px auto 150px auto',
+  },
+
+  loginPStyle: {
+    margin: '3rem 0rem 0rem 2rem',
+  },
+
+  loginFormStyle: {
+    margin: '1rem 0rem 2rem 2rem',
+  },
+})
 
 export default Login;
