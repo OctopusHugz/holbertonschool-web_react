e73f16bfd69f825b7f6411b1513d82b2af294460
OctopusHugz <colsonscott53@gmail.com>
2021-07-15 17:58:50 +0000

    Added keyframes animations. Need to make menuItem hide when Notifications are visible

--- 0x05-React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
+++ 0x05-React_inline_styling/task_4/dashboard/src/Notifications/Notifications.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import NotificationItem from "./NotificationItem";
 import icon from "../assets/close-icon.png";
 import PropTypes from "prop-types";
 import NotificationItemShape from "./NotificationItemShape";
-import { StyleSheet, css } from 'aphrodite';
+import { StyleSheet, css } from "aphrodite";
 
 export default class Notifications extends Component {
   constructor(props) {
@@ -13,8 +13,8 @@ export default class Notifications extends Component {
 
   static propTypes = {
     displayDrawer: PropTypes.bool,
-    listNotifications: PropTypes.arrayOf(NotificationItemShape)
-  }
+    listNotifications: PropTypes.arrayOf(NotificationItemShape),
+  };
 
   static defaultProps = {
     displayDrawer: false,
@@ -22,7 +22,9 @@ export default class Notifications extends Component {
   };
 
   shouldComponentUpdate(nextProps, nextState) {
-    if (nextProps.listNotifications.length > this.props.listNotifications.length)
+    if (
+      nextProps.listNotifications.length > this.props.listNotifications.length
+    )
       return true;
     return false;
   }
@@ -38,70 +40,107 @@ export default class Notifications extends Component {
   render() {
     return (
       <>
-        <div className="menuItem" className={css(styles.menuItemStyle)}>Your notifications</div>
+        <div className="menuItem" className={css(styles.menuItemStyle)}>
+          Your notifications
+        </div>
         {this.props.displayDrawer && (
           <div className={css(styles.notificationPanelStyle)}>
             <div className="Notifications">
               {this.props.listNotifications.length > 0 ? (
                 <>
-                <p style={{ display: "inline" }}>
-                  Here is the list of notifications
-                </p>
-                <button
-                  style={{ float: "right" }}
-                  aria-label="Close"
-                  onClick={this.clickClose}
-                >
-                  <img src={icon} alt="" style={{ height: "3vh" }} />
-                </button>
-                <ul className={css(styles.listStyle)}>
-                  {this.props.listNotifications.map((notification) => (
-                    <NotificationItem
-                      key={notification.id}
-                      type={notification.type}
-                      value={notification.value}
-                      html={notification.html}
-                      markAsRead={() => {this.markAsRead(notification.id)}}
-                    />
-                  ))}
+                  <p style={{ display: "inline" }}>
+                    Here is the list of notifications
+                  </p>
+                  <button
+                    style={{ float: "right" }}
+                    aria-label="Close"
+                    onClick={this.clickClose}
+                  >
+                    <img src={icon} alt="" style={{ height: "3vh" }} />
+                  </button>
+                  <ul className={css(styles.listStyle)}>
+                    {this.props.listNotifications.map((notification) => (
+                      <NotificationItem
+                        key={notification.id}
+                        type={notification.type}
+                        value={notification.value}
+                        html={notification.html}
+                        markAsRead={() => {
+                          this.markAsRead(notification.id);
+                        }}
+                      />
+                    ))}
                   </ul>
-                  </>
-                  ) : <p>No new notification for now</p>
-            }
+                </>
+              ) : (
+                <p>No new notification for now</p>
+              )}
             </div>
           </div>
         )}
       </>
-    )
+    );
   }
 }
 
+const opacityKeyframes = {
+  from: {
+    opacity: 0.5,
+  },
+
+  to: {
+    opacity: 1,
+  },
+};
+
+const translateKeyframes = {
+  "0%": {
+    transform: "translateY(0)",
+  },
+
+  "50%": {
+    transform: "translateY(-5px)",
+  },
+
+  "100%": {
+    transform: "translateY(5px)",
+  },
+};
+
 const styles = StyleSheet.create({
   notificationPanelStyle: {
-    border: '1px red dashed',
-    padding: '1rem',
-    width: '35%',
-    float: 'right',
-    '@media (max-width: 900px)': {
-      border: 'none',
-      height: '100vh',
-      width: '100vw',
-      float: 'none',
+    border: "1px red dashed",
+    padding: "1rem",
+    width: "35%",
+    float: "right",
+    "@media (max-width: 900px)": {
+      border: "none",
+      height: "100vh",
+      width: "100vw",
+      float: "none",
       padding: 0,
       fontSize: 20,
-    }
+    },
   },
 
   menuItemStyle: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    '@media (max-width: 900px)': {
-      display: 'none',
-    }
+    display: "flex",
+    justifyContent: "flex-end",
+    float: "right",
+    backgroundColor: "#fff8f8",
+    "@media (max-width: 900px)": {
+      display: "none",
+    },
+    ":hover": {
+      cursor: "pointer",
+      animationName: [opacityKeyframes, translateKeyframes],
+      animationDuration: '1s, 500ms',
+      animationIterationCount: '3',
+    },
   },
 
   listStyle: {
-    listStyle: 'none',
-    padding: 0
-  }
-})
+    listStyle: "none",
+    padding: 0,
+  },
+});
