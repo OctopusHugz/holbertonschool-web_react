7e6a3c14322da04a1efa6bb8a82f692a80755981
OctopusHugz <colsonscott53@gmail.com>
2021-07-19 20:15:08 +0000

    Added tests for checking submit button disabled by default and then enabled when email and password are not empty

--- 0x06-react_state/task_1/dashboard/src/Login/Login.test.js
+++ 0x06-react_state/task_1/dashboard/src/Login/Login.test.js
@@ -13,7 +13,19 @@ describe('<Login />', () => {
 
 	it('renders a <Login /> component and checks contents', () => {
     const wrapper = shallow(<Login />);
-    expect(wrapper.find('div.App-login input')).toHaveLength(2);
+    expect(wrapper.find('div.App-login input')).toHaveLength(3);
     expect(wrapper.find('div.App-login label')).toHaveLength(2);
+	});
+
+	it('verifies that the submit button is disabled by default', () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.find({ type: 'submit' }).props().disabled).toBe(true);
+	});
+
+	it('verifies that after changing the value of the two inputs, the button is enabled', () => {
+    const wrapper = shallow(<Login />);
+    wrapper.find({ name: 'email' }).simulate('change', { target: { name: 'email', value: 'thedudeabides@lebowski.com' } });
+    wrapper.find({ name: 'password' }).simulate('change', { target: { name: 'password', value: 'markazeronextframedude' } });
+    expect(wrapper.find({ type: 'submit' }).props().disabled).toBe(false);
 	});
 });
