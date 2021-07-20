85019f496ad3bca733e72693f00e4bd6c6dc91f8
OctopusHugz <colsonscott53@gmail.com>
2021-07-20 18:44:12 +0000

    Refactored Footer tests after adding Context.Consumer. Added tests to verify Contact Us is displayed correctly

--- 0x06-react_state/task_3/dashboard/src/Footer/Footer.test.js
+++ 0x06-react_state/task_3/dashboard/src/Footer/Footer.test.js
@@ -1,7 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Footer from './Footer';
 import { StyleSheetTestUtils } from 'aphrodite';
+import { AppContext } from '../App/AppContext';
 
 StyleSheetTestUtils.suppressStyleInjection();
 
@@ -12,7 +16,18 @@ describe('<Footer />', () => {
 	});
 
 	it('renders a <Footer /> component and checks contents', () => {
-    const wrapper = shallow(<Footer />);
+    const wrapper = mount(<Footer />);
     expect(wrapper.find('footer p').text()).toContain('Copyright');
+	});
+
+	it('verifies that the link is not displayed when the user is logged out within the context', () => {
+    const wrapper = mount(<Footer />);
+    expect(wrapper.find('footer p').length).toBe(1);
+	});
+
+  it('verifies that the link is displayed when the user is logged in within the context', () => {
+    const value = { user: { email: 'thedude@aim.com', password: 'thedudeabides', isLoggedIn: true }, logOut: () => { } }
+    const wrapper = mount(<AppContext.Provider value={value}><Footer /></AppContext.Provider>);
+    expect(wrapper.find('footer a').text()).toContain('Contact Us');
 	});
 });
