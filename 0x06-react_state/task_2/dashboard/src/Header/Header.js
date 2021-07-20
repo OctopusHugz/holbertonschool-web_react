6a1b8227af1a73f59760db34735205652b1f9954
OctopusHugz <colsonscott53@gmail.com>
2021-07-20 17:07:36 +0000

    Changed Header to class component. Added context and logoutSection to Header

--- 0x06-react_state/task_2/dashboard/src/Header/Header.js
+++ 0x06-react_state/task_2/dashboard/src/Header/Header.js
@@ -1,33 +1,47 @@
-import React from "react";
-import logo from '../assets/holberton-logo.jpg';
-import { StyleSheet, css } from 'aphrodite';
+import React, { Component } from "react";
+import logo from "../assets/holberton-logo.jpg";
+import { StyleSheet, css } from "aphrodite";
+import AppContext from "../App/AppContext";
 
-const Header = () => {
-  return (
-    <header className={css(styles.headerStyle)}>
-      <img src={logo} className={css(styles.logoStyle)} alt="logo" />
-      <h1 className={css(styles.titleStyle)}>School dashboard</h1>
-    </header>
-  );
-};
+export default class Header extends Component {
+  render() {
+    let value = this.context;
+    return (
+      <>
+        <header className={css(styles.headerStyle)}>
+          <img src={logo} className={css(styles.logoStyle)} alt="logo" />
+          <h1 className={css(styles.titleStyle)}>School dashboard</h1>
+        </header>
+        <section id="logoutSection">
+          {value.user.isLoggedIn && (
+            <h1>Welcome {value.user.email} <a className={css(styles.italicText)} onClick={value.logOut}>(logout)</a></h1>
+          )}
+        </section>
+      </>
+    );
+  }
+}
+Header.contextType = AppContext;
 
 const styles = StyleSheet.create({
   headerStyle: {
-    display: 'flex',
-    alignItems: 'center',
-    height: '25vh',
+    display: "flex",
+    alignItems: "center",
+    height: "25vh",
     minHeight: 150,
   },
 
   logoStyle: {
-    height: '100%',
+    height: "100%",
   },
 
   titleStyle: {
-    color: '#E0344B',
-    fontSize: '2.5rem',
-    marginLeft: '1rem',
-  }
-})
+    color: "#E0344B",
+    fontSize: "2.5rem",
+    marginLeft: "1rem",
+  },
 
-export default Header;
+  italicText: {
+    fontStyle: 'italic',
+  }
+});
