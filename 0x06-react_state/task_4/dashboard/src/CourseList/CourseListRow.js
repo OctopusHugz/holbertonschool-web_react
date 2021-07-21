00a885ff47cc5fa5eb14158f5e666fa3208a2493
OctopusHugz <colsonscott53@gmail.com>
2021-07-21 16:40:23 +0000

    Added useState hook to CourseListRow component for rowChecked styling

--- 0x06-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
+++ 0x06-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -1,22 +1,40 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
 
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+  const [isChecked, setIsChecked] = useState(false);
   return (
-    <tr className={isHeader ? css(styles.headerRowStyle) : css(styles.defaultRowStyle)}>
+    <tr
+      className={
+        isHeader ? css(styles.headerRowStyle) : css(styles.defaultRowStyle)
+      }
+    >
       {isHeader && textSecondCell === null && (
-        <th colSpan={2} className={css(styles.thBorderBottom)}>{textFirstCell}</th>
+        <th colSpan={2} className={css(styles.thBorderBottom)}>
+          {textFirstCell}
+        </th>
       )}
       {isHeader && textSecondCell !== null && (
         <>
-          <th className={css(styles.textAlignLeft, styles.thBorderBottom)}>{textFirstCell}</th>
-          <th className={css(styles.textAlignLeft, styles.thBorderBottom)}>{textSecondCell}</th>
+          <th className={css(styles.textAlignLeft, styles.thBorderBottom)}>
+            {textFirstCell}
+          </th>
+          <th className={css(styles.textAlignLeft, styles.thBorderBottom)}>
+            {textSecondCell}
+          </th>
         </>
       )}
       {!isHeader && (
         <>
-          <td>{textFirstCell}</td>
+          <td className={isChecked ? css(styles.rowChecked) : "rowNotChecked"}>
+            <input
+              type="checkbox"
+              name="styleCheckbox"
+              onChange={() => {setIsChecked(!isChecked)}}
+            />
+            {textFirstCell}
+          </td>
           <td>{textSecondCell}</td>
         </>
       )}
@@ -27,33 +45,34 @@ const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
   textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ])
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 CourseListRow.defaultProps = {
   isHeader: false,
-  textSecondCell: null
+  textSecondCell: null,
 };
 
 const styles = StyleSheet.create({
   headerRowStyle: {
-    backgroundColor: '#deb5b545',
+    backgroundColor: "#deb5b545",
   },
 
   defaultRowStyle: {
-    backgroundColor: '#f5f5f5ab',
+    backgroundColor: "#f5f5f5ab",
   },
 
   textAlignLeft: {
-    textAlign: 'left',
+    textAlign: "left",
   },
 
   thBorderBottom: {
-    borderBottom: '1px solid grey',
+    borderBottom: "1px solid grey",
   },
-})
+
+  rowChecked: {
+    backgroundColor: "#e6e4e4",
+  },
+});
 
 export default CourseListRow;
