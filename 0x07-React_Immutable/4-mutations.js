5eba688ee8b39f31d52704d0409cfaea66dd2a19
OctopusHugz <colsonscott53@gmail.com>
2021-07-26 16:13:53 +0000

    Added 4-mutations.js

---
+++ 0x07-React_Immutable/4-mutations.js
@@ -0,0 +1,22 @@
+const { List, Map } = require("immutable");
+const map = Map({
+  1: "Liam",
+  2: "Noah",
+  3: "Elijah",
+  4: "Oliver",
+  5: "Jacob",
+  6: "Lucas",
+});
+const map2 = List(map).set(2, "Benjamin").set(4, "Oliver");
+
+// Adding other possibilities since task requirement is so vague
+
+// const map2 = map.set('2', 'Benjamin').set('4', 'Oliver');
+
+// const map2 = map.toIndexedSeq().set(2, ['Benjamin']).set(4, ['Oliver']);
+
+// const map2 = map.toIndexedSeq().toArray();
+// map2[2] = 'Benjamin';
+// map2[4] = 'Oliver';
+
+module.exports = { map, map2 };
