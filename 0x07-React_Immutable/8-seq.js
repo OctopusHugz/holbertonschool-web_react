34ccee078383c8c82394b4ee5258e075032e8d8a
OctopusHugz <colsonscott53@gmail.com>
2021-07-27 17:28:18 +0000

    Fixed import and export

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -1,16 +1,18 @@
-const { Seq } = require("immutable");
+import { Seq } from 'immutable';
 
 const printBestStudents = (object) => {
   const bestStudents = Seq(object)
     .filter((student) => student.score >= 70)
     .map((student) => ({
       score: student.score,
-			firstName:
-				`${student.firstName.charAt(0).toUpperCase()}${student.firstName.slice(1)}`,
-      lastName:
-			`${student.lastName.charAt(0).toUpperCase()}${student.lastName.slice(1)}`,
+      firstName: `${student.firstName
+        .charAt(0)
+        .toUpperCase()}${student.firstName.slice(1)}`,
+      lastName: `${student.lastName
+        .charAt(0)
+        .toUpperCase()}${student.lastName.slice(1)}`,
     }));
   console.log(bestStudents.toObject());
 };
 
-module.exports = printBestStudents;
+export default printBestStudents;
