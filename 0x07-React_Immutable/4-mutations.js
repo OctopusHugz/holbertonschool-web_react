005dbc77b5fd510d46cc60ce94744cc656dd059d
OctopusHugz <colsonscott53@gmail.com>
2021-07-27 17:23:57 +0000

    Removed extraneous comments

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -12,14 +12,4 @@ const map2 = List(map).withMutations((list) => {
   list.set(2, 'Benjamin').set(4, 'Oliver');
 });
 
-// Adding other possibilities since task requirement is so vague
-
-// const map2 = map.set('2', 'Benjamin').set('4', 'Oliver');
-
-// const map2 = map.toIndexedSeq().set(2, ['Benjamin']).set(4, ['Oliver']);
-
-// const map2 = map.toIndexedSeq().toArray();
-// map2[2] = 'Benjamin';
-// map2[4] = 'Oliver';
-
 export { map, map2 };
