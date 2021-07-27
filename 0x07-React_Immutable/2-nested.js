6011e6d49e4d9fbb899801ae77148ec91f7fa124
OctopusHugz <colsonscott53@gmail.com>
2021-07-27 17:32:49 +0000

    Removd Map explicit call

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,5 +1,5 @@
-import { Map } from 'immutable';
+import { getIn, fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  return Map.getIn(object, array);
+  return getIn(fromJS(object), array);
 }
