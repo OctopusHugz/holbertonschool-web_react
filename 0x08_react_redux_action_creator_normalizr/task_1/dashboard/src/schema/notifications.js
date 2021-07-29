caf9d8fa25c18eab687e0a2b6bc17dbc85986882
OctopusHugz <colsonscott53@gmail.com>
2021-07-29 16:28:50 +0000

    Added normalized notifications and tests for verifying entities

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,7 +1,19 @@
 import notifications from "../../../../notifications.json";
+import { normalize, schema } from "normalizr";
 
-export default function getAllNotificationsByUser(userId) {
+const user = new schema.Entity("users");
+const message = new schema.Entity("messages", {}, { idAttribute: "guid" });
+const notification = new schema.Entity("notifications", {
+  author: user,
+  context: message,
+});
+const mySchema = [notification];
+const normalizedNotifications = normalize(notifications, mySchema);
+
+function getAllNotificationsByUser(userId) {
   return notifications
     .filter((notification) => notification.author.id === userId)
     .map((notification) => notification.context);
 }
+
+export { normalizedNotifications, getAllNotificationsByUser };
