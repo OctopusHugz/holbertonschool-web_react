c6f7d9e7339366305c6c7da3956959150b61dce2
OctopusHugz <colsonscott53@gmail.com>
2021-08-05 22:03:55 +0000

    Finally got fetch to return error using fetch-mock-jest package

--- 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x08_react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -9,7 +9,23 @@ import {
   logout,
   displayNotificationDrawer,
   hideNotificationDrawer,
+  loginSuccess,
+  loginFailure,
+  loginRequest,
 } from "./uiActionCreators";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+// import fetchMock from "fetch-mock-jest";
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+const store = mockStore({});
+const user = {
+  email: "dude@lebowski.com",
+  password: "thatrugtiedtheroomtogether",
+};
+jest.mock("node-fetch", () => require("fetch-mock-jest").sandbox());
+const fetchMock = require("node-fetch");
 
 describe("uiActions", () => {
   it("should return correct value for login action creator", () => {
@@ -36,5 +52,31 @@ describe("uiActions", () => {
   it("should return correct value for hideNotificationDrawer action creator", () => {
     const result = hideNotificationDrawer();
     expect(result).toEqual({ type: HIDE_NOTIFICATION_DRAWER });
+  });
+});
+
+describe("loginRequest", () => {
+  it("should verify store received LOGIN and LOGIN_SUCCESS actions", () => {
+    fetchMock.get("http://localhost:8564/login-success.json", 200);
+    return store.dispatch(loginRequest(user.email, user.password)).then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual(login(user.email, user.password));
+      expect(actions[1]).toEqual(loginSuccess());
+    });
+  });
+
+  it("should verify store received LOGIN and LOGIN_FAILURE actions", () => {
+    fetchMock.get(
+      "http://localhost:8564/login-success.json",
+      { throws: new Error("fetch failed") },
+      {
+        overwriteRoutes: true,
+      }
+    );
+    return store.dispatch(loginRequest(user.email, user.password)).then(() => {
+      const actions = store.getActions();
+      expect(actions[2]).toEqual(login(user.email, user.password));
+      expect(actions[3]).toEqual(loginFailure());
+    });
   });
 });
