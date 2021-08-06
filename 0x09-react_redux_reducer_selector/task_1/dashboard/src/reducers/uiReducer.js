439904ca0a7e5c6aceadc48a7422b6e2479f9dbf
OctopusHugz <colsonscott53@gmail.com>
2021-08-06 16:18:55 +0000

    Updated uiReducer.js and uiReducer.test.js to use Immutable Map

--- 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -5,44 +5,30 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAILURE,
 } from "../actions/uiActionTypes";
+import { Map } from "immutable";
 
-const initialState = {
+const initialState = Map({
   isNotificationDrawerVisible: false,
   isUserLoggedIn: false,
   user: {},
-};
+});
 
 export default function uiReducer(state = initialState, action) {
   switch (action) {
     case DISPLAY_NOTIFICATION_DRAWER: {
-      return {
-        ...state,
-        isNotificationDrawerVisible: true,
-      };
+      return initialState.set("isNotificationDrawerVisible", true);
     }
     case HIDE_NOTIFICATION_DRAWER: {
-      return {
-        ...state,
-        isNotificationDrawerVisible: false,
-      };
+      return initialState.set("isNotificationDrawerVisible", false);
     }
     case LOGIN_SUCCESS: {
-      return {
-        ...state,
-        isUserLoggedIn: true,
-      };
+      return initialState.set("isUserLoggedIn", true);
     }
     case LOGIN_FAILURE: {
-      return {
-        ...state,
-        isUserLoggedIn: false,
-      };
+      return initialState.set("isUserLoggedIn", false);
     }
     case LOGOUT: {
-      return {
-        ...state,
-        isUserLoggedIn: false,
-      };
+      return initialState.set("isUserLoggedIn", false);
     }
     default:
       return state;
