db34467d4c716f3af69b7da8ae2f6830e72f9ea9
OctopusHugz <colsonscott53@gmail.com>
2021-08-14 16:29:29 +0000

    Refactored Footer tests after connecting component

--- 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
+++ 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
@@ -1,33 +1,37 @@
 /**
  * @jest-environment jsdom
  */
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import Footer from './Footer';
-import { StyleSheetTestUtils } from 'aphrodite';
-import { AppContext } from '../App/AppContext';
+import React from "react";
+import { shallow } from "enzyme";
+import Footer from "./Footer";
+import { StyleSheetTestUtils } from "aphrodite";
+import { AppContext } from "../App/AppContext";
 
 StyleSheetTestUtils.suppressStyleInjection();
 
-describe('<Footer />', () => {
-  it('renders a <Footer /> component', () => {
-    const wrapper = shallow(<Footer />);
+const loggedInUser = {
+  email: "thedude@lebowski.com",
+  password: "thedudeabides",
+};
+
+describe("<Footer />", () => {
+  it("renders a <Footer /> component", () => {
+    const wrapper = shallow(<Footer user={loggedInUser} />);
     expect(wrapper).toHaveLength(1);
-	});
+  });
 
-	it('renders a <Footer /> component and checks contents', () => {
-    const wrapper = mount(<Footer />);
-    expect(wrapper.find('footer p').text()).toContain('Copyright');
-	});
+  it("renders a <Footer /> component and checks contents", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find("footer p").text()).toContain("Copyright");
+  });
 
-	it('verifies that the link is not displayed when the user is logged out within the context', () => {
-    const wrapper = mount(<Footer />);
-    expect(wrapper.find('footer p').length).toBe(1);
-	});
+  it("verifies that the link is not displayed when the user is logged out within the context", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find("footer p").length).toBe(1);
+  });
 
-  it('verifies that the link is displayed when the user is logged in within the context', () => {
-    const value = { user: { email: 'thedude@aim.com', password: 'thedudeabides', isLoggedIn: true }, logOut: () => { } }
-    const wrapper = mount(<AppContext.Provider value={value}><Footer /></AppContext.Provider>);
-    expect(wrapper.find('footer a').text()).toContain('Contact Us');
-	});
+  it("verifies that the link is displayed when the user is logged in within the context", () => {
+    const wrapper = shallow(<Footer user={loggedInUser} />);
+    expect(wrapper.find("footer a").text()).toContain("Contact Us");
+  });
 });
