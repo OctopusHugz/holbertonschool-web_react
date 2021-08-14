65cf29b3a2c043f903b75d69a1e1f64d00773985
OctopusHugz <colsonscott53@gmail.com>
2021-08-14 02:27:37 +0000

    Added thunk middleware to store

--- 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
+++ 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/index.js
@@ -1,14 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App, { ConnectedApp } from "./App/App";
-import { createStore } from "redux";
+import { ConnectedApp } from "./App/App";
+import { applyMiddleware, createStore } from "redux";
 import uiReducer from "./reducers/uiReducer";
 import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-const store = createStore(
-  uiReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(uiReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
