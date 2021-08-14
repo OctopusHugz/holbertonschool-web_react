bd1b11d6a697dd178c28149c533c6e605d8cabbb
OctopusHugz <colsonscott53@gmail.com>
2021-08-14 16:16:20 +0000

    Refactored tests after connecting components

--- 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/App/App.test.js
+++ 0x0A-react_redux_connectors_and_providers/task_2/dashboard/src/App/App.test.js
@@ -2,23 +2,17 @@
  * @jest-environment jsdom
  */
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { shallow } from "enzyme";
 import App, { mapStateToProps } from "./App";
 import Notification from "../Notifications/Notifications";
-import Header from "../Header/Header";
+import { ConnectedHeader } from "../Header/Header";
 import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
+import { ConnectedFooter } from "../Footer/Footer";
 import CourseList from "../CourseList/CourseList";
 import { StyleSheetTestUtils } from "aphrodite";
 import { fromJS } from "immutable";
 
 StyleSheetTestUtils.suppressStyleInjection();
-
-const loggedInUser = {
-  email: "thedude@aim.com",
-  password: "thedudeabides",
-  isLoggedIn: true,
-};
 
 describe("<App />", () => {
   it("renders an <App /> component", () => {
@@ -33,7 +27,7 @@ describe("<App />", () => {
 
   it("checks for a <Header /> component", () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(Header)).toHaveLength(1);
+    expect(wrapper.find(ConnectedHeader)).toHaveLength(1);
   });
 
   it("checks for a <Login /> component", () => {
@@ -43,7 +37,7 @@ describe("<App />", () => {
 
   it("checks for a <Footer /> component", () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(Footer)).toHaveLength(1);
+    expect(wrapper.find(ConnectedFooter)).toHaveLength(1);
   });
 
   it("checks that <CourseList /> component is not displayed", () => {
@@ -52,62 +46,9 @@ describe("<App />", () => {
   });
 
   it("checks component behavior when isLoggedIn === true", () => {
-    const wrapper = shallow(<App />);
-    wrapper.setState({ user: loggedInUser });
+    const wrapper = shallow(<App isLoggedIn={true} />);
     expect(wrapper.find(Login)).toHaveLength(0);
     expect(wrapper.find(CourseList)).toHaveLength(1);
-  });
-
-  it("checks behavior of logOut prop", () => {
-    const map = {};
-    window.addEventListener = jest.fn().mockImplementation((event, cb) => {
-      map[event] = cb;
-    });
-    window.alert = jest.fn();
-
-    const wrapper = mount(<App />);
-    wrapper.setState({ user: loggedInUser });
-    map.keydown({ ctrlKey: true, key: "h" });
-    expect(window.alert).toHaveBeenCalledWith("Logging you out");
-    expect(wrapper.state().user.email).toBe("");
-    expect(wrapper.state().user.password).toBe("");
-    expect(wrapper.state().user.isLoggedIn).toBe(false);
-    window.alert.mockRestore();
-  });
-
-  // it("verifies default state for displayDrawer === false", () => {
-  //   const wrapper = shallow(<App />);
-  //   expect(wrapper.state().displayDrawer).toBe(false);
-  // });
-
-  // it("verifies that after calling handleDisplayDrawer, the state === true", () => {
-  //   const wrapper = shallow(<App />);
-  //   wrapper.instance().handleDisplayDrawer();
-  //   expect(wrapper.state().displayDrawer).toBe(true);
-  // });
-
-  // it("verifies that after calling handleHideDrawer, the state === false", () => {
-  //   const wrapper = shallow(<App />);
-  //   wrapper.setState({ displayDrawer: true });
-  //   wrapper.instance().handleHideDrawer();
-  //   expect(wrapper.state().displayDrawer).toBe(false);
-  // });
-
-  it("verifies that the logIn function updates the state correctly", () => {
-    const wrapper = shallow(<App />);
-    wrapper.instance().logIn(loggedInUser.email, loggedInUser.password);
-    expect(wrapper.state().user.email).toBe("thedude@aim.com");
-    expect(wrapper.state().user.password).toBe("thedudeabides");
-    expect(wrapper.state().user.isLoggedIn).toBe(true);
-  });
-
-  it("verifies that the logOut function updates the state correctly", () => {
-    const wrapper = shallow(<App />);
-    wrapper.setState({ user: loggedInUser });
-    wrapper.state().logOut();
-    expect(wrapper.state().user.email).toBe("");
-    expect(wrapper.state().user.password).toBe("");
-    expect(wrapper.state().user.isLoggedIn).toBe(false);
   });
 
   it("verifies that markNotificationAsRead removes notification from listNotifications in state", () => {
