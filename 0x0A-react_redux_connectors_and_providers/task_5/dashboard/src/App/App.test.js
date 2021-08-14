235be2a1fd6062268fe84d44c3b109f61e7db539
OctopusHugz <colsonscott53@gmail.com>
2021-08-14 21:13:43 +0000

    Changed test to look for ConnectedNotifications and removed markNotificationAsRead test after deleting method

--- 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/App/App.test.js
+++ 0x0A-react_redux_connectors_and_providers/task_5/dashboard/src/App/App.test.js
@@ -4,7 +4,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App, { mapStateToProps } from "./App";
-import Notification from "../Notifications/Notifications";
+import { ConnectedNotifications } from "../Notifications/Notifications";
 import { ConnectedHeader } from "../Header/Header";
 import Login from "../Login/Login";
 import { ConnectedFooter } from "../Footer/Footer";
@@ -22,7 +22,7 @@ describe("<App />", () => {
 
   it("checks for a <Notifications /> component", () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(Notification)).toHaveLength(1);
+    expect(wrapper.find(ConnectedNotifications)).toHaveLength(1);
   });
 
   it("checks for a <Header /> component", () => {
@@ -49,22 +49,6 @@ describe("<App />", () => {
     const wrapper = shallow(<App isLoggedIn={true} />);
     expect(wrapper.find(Login)).toHaveLength(0);
     expect(wrapper.find(CourseList)).toHaveLength(1);
-  });
-
-  it("verifies that markNotificationAsRead removes notification from listNotifications in state", () => {
-    const wrapper = shallow(<App />);
-    wrapper.setState({
-      listNotifications: [
-        { id: 1, type: "default", value: "New course available" },
-        { id: 2, type: "urgent", value: "New resume available" },
-        { id: 3, type: "urgent", value: "New majors available" },
-      ],
-    });
-    wrapper.instance().markNotificationAsRead(2);
-    expect(wrapper.state().listNotifications).toEqual([
-      { id: 1, type: "default", value: "New course available" },
-      { id: 3, type: "urgent", value: "New majors available" },
-    ]);
   });
 });
 
